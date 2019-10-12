12adec4db7f419da86904fc65a1407611061a0fa
Cristian <easyservice369@gmail.com>
2019-10-12 11:31:40 -0500

    se implemento pagina de not found

---
+++ src/components/Page404.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/components/Products.styl';
+
+const Page404 = () => {
+  return <div className="Products">Pagina no encontrada</div>;
+};
+
+export default Page404;
